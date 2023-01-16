f0fba7d241d43f97ea19b33b4f87593cb90b0f7d
ckschlosser <ckschlosser13@gmail.com>
2023-01-16 11:10:44 -0600

    in view animation fade in added to content sections

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,32 +1,45 @@
-<script>
+<script lang="ts">
 	import RomanTitle from './RomanTitle.svelte';
 	import selfie from '$lib/images/selfie.jpg';
+	import { fade } from 'svelte/transition';
+	import { inview } from 'svelte-inview';
+	let isInView: Boolean;
 	let title = '';
 </script>
 
-<section class="mb-20" id="about">
-	<RomanTitle title="About Me" />
-	<div class="grid grid-cols-1 md:grid-cols-2">
-		<div class="text-cream">
-			<p class="pt-10">
-				Hello! My name is Clinton Schlosser and I am a self taught front end web developer. I have
-				had an interest in web development coming out of high school but decided to go a different
-				route in college. I became an Athletic Trainer for six years and finally decided that I
-				wanted to dive into web development full time. I have been learning HTML and CSS since 2019.
-			</p>
-			<p class="pt-4">
-				During 2021, I dove into JavaScript, Svelte, and React. I have been lucky enough to have
-				brothers that are in the industry and have been helping me along the way. My main focus
-				these days is to continue to grow in these languages and one day start into back end web
-				development.
-			</p>
-			<p class="pt-4">
-				I also have had some experience with building my own database on Supabase and running one of
-				my applications through it using Svelte.
-			</p>
+<section
+	class="mb-20"
+	id="about"
+	use:inview={{ unobserveOnEnter: true, rootMargin: '-35%' }}
+	on:change={({ detail }) => {
+		isInView = detail.inView;
+	}}
+>
+	{#if isInView}
+		<RomanTitle title="About Me" />
+		<div in:fade class="grid grid-cols-1 md:grid-cols-2">
+			<div class="text-cream">
+				<p class="pt-10">
+					Hello! My name is Clinton Schlosser and I am a self taught front end web developer. I have
+					had an interest in web development coming out of high school but decided to go a different
+					route in college. I became an Athletic Trainer for six years and finally decided that I
+					wanted to dive into web development full time. I have been learning HTML and CSS since
+					2019.
+				</p>
+				<p class="pt-4">
+					During 2021, I dove into JavaScript, Svelte, and React. I have been lucky enough to have
+					brothers that are in the industry and have been helping me along the way. My main focus
+					these days is to continue to grow in these languages and one day start into back end web
+					development.
+				</p>
+				<p class="pt-4">
+					I also have had some experience with building my own database on Supabase and running one
+					of my applications through it using Svelte.
+				</p>
+			</div>
+			<div class="place-self-center mt-6">
+				<img class="w-auto h-auto rounded-full" src={selfie} alt="Selfie" />
+			</div>
 		</div>
-		<div class="place-self-center mt-6">
-			<img class="w-auto h-auto rounded-full" src={selfie} alt="Selfie" />
-		</div>
-	</div>
+	{/if}
 </section>
--- src/lib/components/Skills.svelte
+++ src/lib/components/Skills.svelte
@@ -1,14 +1,27 @@
-<script>
+<script lang="ts">
 	import Icons from './Icons.svelte';
 	import RomanTitle from './RomanTitle.svelte';
+	import { fade } from 'svelte/transition';
+	import { inview } from 'svelte-inview';
+	let isInView: Boolean;
 	let title = '';
 </script>
 
-<section class="px-8" id="skills">
-	<RomanTitle title="Skills" />
-	<div
-		class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 md:gap-16 md:gap-y-14 mt-12 justify-items-center items-center"
-	>
-		<Icons />
-	</div>
+<section
+	class="px-8"
+	id="skills"
+	use:inview={{ unobserveOnEnter: true, rootMargin: '-35%' }}
+	on:change={({ detail }) => {
+		isInView = detail.inView;
+	}}
+>
+	{#if isInView}
+		<RomanTitle title="Skills" />
+		<div
+			in:fade
+			class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 md:gap-16 md:gap-y-14 mt-12 justify-items-center items-center"
+		>
+			<Icons />
+		</div>
+	{/if}
 </section>
